8b64ce23fcea19b311b4b79d41da22e2f2b916f8
Tata_kn18 <karmikday@gmail.com>
2022-03-19 16:48:02 +0200

    update

--- mian.cpp
+++ mian.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 using namespace std;
+const int N = 8;
+int desk[N][N];
+int steps[8][2] = { {1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2} };
+bool Horse(int x,int y)
+{
+	desk[x][y] = 10;
+	int x1, y1, j;
+	j = 0;
+	for (int i = 0; i < N; i++)
+	{
+		x1 = x + steps[i][0];
+		y1 = y + steps[i][1];
+		if ((x1<0) || (x1 >= N) || (y1 < 0) || (y1 >= N))
+		{
+			continue;
+		}
+		j++;
+		desk[x1][y1] = j;
+	}
+	return false;
+}
+void PrintDesk()
+{
+for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			printf(" %2d", desk[i][j]);
+		}
+		    cout << "\n";
+	}
+}
 int main()
 {
-	const int N = 8;
-	int desk[N][N];
-	int steps[8][2] = { {1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2} };
+	int PointX,PointY;
+
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
@@ -12,12 +43,12 @@ int main()
 			desk[i][j] = 0;
 		}
 	}
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cout << desk[i][j] << " ";
-		}
-		    cout << "\n";
-	}
+	cout << "First horizontal point(1-8)->";
+	cin >> PointX;
+	cout << "First vertical point(1-8)->";
+	cin >> PointY;
+	cout << endl;
+	Horse(PointX, PointY);
+	PrintDesk();
+	
 }
