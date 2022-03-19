49dc751384df5f65e8806547f658288ce2b6a0aa
Tata_kn18 <karmikday@gmail.com>
2022-03-19 16:17:41 +0200

    update

--- mian.cpp
+++ mian.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	return 0;
+	const int N = 8;
+	int desk[N][N];
+	int steps[8][2] = { {1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2} };
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			desk[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << desk[i][j] << " ";
+		}
+		    cout << "\n";
+	}
 }
