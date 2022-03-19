cb71c1d8fb150f4d234ca5235f14f73f61034e09
Tata_kn18 <karmikday@gmail.com>
2022-03-19 17:27:09 +0200

    update

--- mian.cpp
+++ mian.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 int main()
 {
-	return 0;
+	int map[3][3] = { 0 };
+	int x = 0,y = 0;
+	while (true)
+	{
+		cout << "Enter coordinate->";
+		cin >> x >> y;	
+		map[x][y] = 1;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cout << map[j][i];
+			}
+			cout << endl;
+		}
+	}
 }
