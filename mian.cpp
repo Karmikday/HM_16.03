e0291e1748cd110ed010f8c1c85cf40fc1db3d50
Tata_kn18 <karmikday@gmail.com>
2022-03-19 17:58:22 +0200

    update

--- mian.cpp
+++ mian.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	int map[3][3] = { 0 };
 	int x = 0,y = 0;
+	int c = 0, c1 = 0, c2 = 0,c3=0;
 	while (true)
 	{
 		cout << "Enter coordinate->";
@@ -16,6 +17,37 @@ int main()
 				cout << map[j][i];
 			}
 			cout << endl;
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (map[j][i] == 1)
+				{
+					c++;
+				}
+				if (map[i][j] == 1)
+				{
+					c1++;
+				}
+				if (map[j][j] == 1)
+				{
+					c2++;
+				}
+				if (map[2-j][j] == 1)
+				{
+					c3++;
+				}
+			}
+			if (c == 3 || c1 == 3 || c2 == 3 || c3 == 3)
+			{
+				cout << "You Win!<3" << endl;
+				break;
+			}
+			c = 0;
+			c1 = 0, 
+			c2 = 0;
+			c3 = 0;
 		}
 	}
 }
