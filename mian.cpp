25260c557b7ea2fcf2e20e85fd4541f1fd69c379
Tata_kn18 <karmikday@gmail.com>
2022-03-19 18:39:14 +0200

    done

--- mian.cpp
+++ mian.cpp
@@ -13,22 +13,22 @@ void ShowMap(int map[3][3])
 			cout << endl;
 	}
 }
-bool Game(int map[3][3])
+bool Game(int map[3][3],int val)
 {
 int c = 0, c1 = 0, c2 = 0, c3 = 0;
 for (int i = 0; i < 3; i++)
 {
 	for (int j = 0; j < 3; j++)
 	{
-		if (map[j][i] == 1)
+		if (map[j][i] == val)
 		{
 			c++;
 		}
-		if (map[i][j] == 1)
+		if (map[i][j] == val)
 		{
 			c1++;
 		}
-		if (map[j][j] == 1)
+		if (map[j][j] == val)
 		{
 			c2++;
 		}
@@ -49,42 +49,6 @@ for (int i = 0; i < 3; i++)
 }
     return false ;
 }
-bool Game2(int map[3][3])
-{
-	int c = 0, c1 = 0, c2 = 0, c3 = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (map[j][i] == 2)
-			{
-				c++;
-			}
-			if (map[i][j] == 2)
-			{
-				c1++;
-			}
-			if (map[j][j] == 2)
-			{
-				c2++;
-			}
-			if (map[2 - j][j] == 2)
-			{
-				c3++;
-			}
-		}
-		if (c == 3 || c1 == 3 || c2 == 3 || c3 == 3)
-		{
-			return true;
-			break;
-		}
-		c = 0;
-		c1 = 0,
-		c2 = 0;
-		c3 = 0;
-	}
-	return false;
-}
 int main()
 {
 	int map[3][3] = { 0 };
@@ -92,15 +56,15 @@ int main()
 	int x = 0,y = 0;
 	while (true)
 	{
-	/*	cout << "Enter coordinate->";
+		cout << "Enter coordinate->";
 		cin >> x >> y;	
 		map[x][y] = 1;
 		ShowMap(map);
-		if (Game(map) == true)  
+		if (Game(map,1) == true)  
 		{
 			cout << "Win!" << endl;
 			break;
-		}*/
+		}
 		cout << "Computer move" << endl;
 		do
 		{
@@ -109,12 +73,11 @@ int main()
 		}while (map[x][y] != 0);
 		map[x][y] = 2; 
 		ShowMap(map);
-		if (Game2(map) == true)
+		if (Game(map,2) == true)
 		{
 			cout << "Lose!" << endl;
 			break;
 		}
-		_getch();
 	}
 	system("pause");
 	return 0;
